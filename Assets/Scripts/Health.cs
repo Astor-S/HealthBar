602bc5f728914d48ad7fbd3bb8d5187e3452e23a
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-09-06 01:59:49 +0300

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _maxValue;
+    private float _minValue = 0;
+
+    public event Action<float, float> ValueChanged;
+
+    public float Value { get; private set; }
+    public float MaxValue => _maxValue;
+    public float CurrentHealth => Value;
+    public bool IsAlive => CurrentHealth > 0;
+
+    private void Start()
+    {
+        Value = MaxValue;
+    }
+
+    public void Heal(float value)
+    {
+        if (value < 0)
+            throw new ArgumentOutOfRangeException(nameof(value));
+
+        float newHealth = Mathf.Min(Value + value, MaxValue);
+        UpdateValue(newHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            throw new ArgumentOutOfRangeException(nameof(damage));
+
+        float newHealth = Mathf.Max(Value - damage, _minValue);
+        UpdateValue(newHealth);
+
+        if (Value <= 0)
+            ApplyDeath();
+    }
+
+    private void UpdateValue(float value)
+    {
+        Value = value;
+        ValueChanged?.Invoke(value, _maxValue);
+    }
+
+    private void ApplyDeath()
+    {
+        if (IsAlive == false)
+            Destroy(gameObject);
+    }
+}
